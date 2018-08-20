c524bfcd179228b90cc05fd4c13e2862f275dbdc
RatoX <rodrigovieira18@gmail.com>
2018-08-20 16:57:59 -0300

    fix lint

--- src/hoc/utils/withPostNotFound.js
+++ src/hoc/utils/withPostNotFound.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { compose, branch, renderComponent } from 'recompose'
